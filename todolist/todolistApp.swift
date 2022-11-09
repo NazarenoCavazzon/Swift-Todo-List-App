2f33a739848b50cd3ccf73ae70331e7b0c8d46e9
Nazareno Cavazzon <cavazzonnazareno@gmail.com>
2022-11-08 23:37:34 -0300

    Initial Commit

---
+++ todolist/todolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  todolistApp.swift
+//  todolist
+//
+//  Created by Nazareno Cavazzon on 08/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct todolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
